c09754d7f396488e24dabac2d1d4a8601bdb3f11
Евгений Гужихин <gpont97@gmail.com>
2017-09-14 15:39:46 +0700

    change gitlab to github

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-wget -O ~/.zshrc https://gitlab.com/gpont/configs/raw/master/.zshrc
-wget -O ~/.zshrc_oh_my_zsh https://gitlab.com/gpont/configs/raw/master/.zshrc_oh_my_zsh
+wget -O ~/.zshrc https://github.com/gpont/configs/raw/master/.zshrc
+wget -O ~/.zshrc_oh_my_zsh https://github.com/gpont/configs/raw/master/.zshrc_oh_my_zsh
 mkdir .config
 mkdir .config/nvim
-wget -O ~/.config/nvim/init.vim https://gitlab.com/gpont/configs/raw/master/init.vim
+wget -O ~/.config/nvim/init.vim https://github.com/gpont/configs/raw/master/init.vim
 git config --global oh-my-zsh.hide-status 1
